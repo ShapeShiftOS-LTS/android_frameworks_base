abebe6503f6ec7746af103b1a5c605b26523621d
Matt Pietal <mpietal@google.com>
2022-08-18 12:04:43 +0000

    [DO NOT MERGE] Do not dismiss keyguard after SIM PUK unlock
    After PUK unlock, multiple calls to
    KeyguardSecurityContainerController#dismiss() were being called from
    the KeyguardSimPukViewController, which begins the transition to the
    next security screen, if any. At the same time, other parts of the
    system, also listening to SIM events, recognize the PUK unlock and
    call KeyguardSecurityContainer#showSecurityScreen, which updates which
    security method comes next. After boot, this should be one of PIN,
    Password, Pattern, assuming they have a security method. If one of the
    first dismiss() calls comes AFTER the security method changes, this is
    incorrectly recognized by the code as a successful
    PIN/pattern/password unlock. This causes the keyguard to be marked as
    done, causing screen flickers and incorrect system state.
    
    The solution: every call to dismiss() should include a new parameter
    for the security method used. If there is a difference between this
    parameter and the current value in KeyguardSecurityContainerCallback,
    ignore the request, as the system state has changed.
    
    Bug: 218500036
    Test: atest KeyguardSecurityContainerTest
    
    Merged-In: I7c8714a177bc85fbce92f6e8fe911f74ca2ac243
    Change-Id: I30226bc7b5eda9480d471b35fe81e106b0491ff8
    (cherry picked from commit c27cf661a6b14849bd13ba69b5ee40c9066fcf2c)
    Merged-In: I30226bc7b5eda9480d471b35fe81e106b0491ff8

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -34,6 +34,7 @@ import java.util.List;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.LockPatternUtils.RequestThrottledException;
 import com.android.internal.widget.LockscreenCredential;
+import com.android.keyguard.KeyguardSecurityModel.SecurityMode;
 import com.android.keyguard.PasswordTextView.QuickUnlockListener;
 import com.android.settingslib.animation.AppearAnimationUtils;
 import com.android.settingslib.animation.DisappearAnimationUtils;
@@ -292,5 +293,10 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
             // do nothing
         }
         return pinPasswordLength >= 4 ? pinPasswordLength : -1;
+    }
+
+    @Override
+    public SecurityMode getSecurityMode() {
+        return SecurityMode.PIN;
     }
 }
